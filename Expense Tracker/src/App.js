9feab0357f3cc3ad5f13d430f991660f4a43d2bb
RAHUL K <76773801+rahul-kjry@users.noreply.github.com>
2022-12-28 00:11:55 +0530

    Add files via upload

---
+++ Expense Tracker/src/App.js
@@ -0,0 +1,42 @@
+import Expenses from "./components/Expenses/Expenses";
+import NewExpense from "./components/NewExpense/NewExpense";
+import { useState } from "react";
+
+const dummyData = [
+  {
+    id: "e1",
+    title: "Car Insurance",
+    amount: 1234,
+    date: new Date(2021, 8, 28),
+  },
+  {
+    id: "e2",
+    title: "Books",
+    amount: 1234,
+    date: new Date(2021, 8, 28),
+  },
+  {
+    id: "e3",
+    title: "Food",
+    amount: 1234,
+    date: new Date(2021, 8, 28),
+  },
+];
+
+function App() {
+  const [expenses, setExpenses] = useState(dummyData);
+  function addExpenseHandler(expense) {
+    setExpenses((prevExpenses) => {
+      return [expense, ...prevExpenses];
+    });
+  }
+  return (
+    <div>
+      <NewExpense onAddExpense={addExpenseHandler} />
+
+      <Expenses items={expenses} />
+    </div>
+  );
+}
+
+export default App;
